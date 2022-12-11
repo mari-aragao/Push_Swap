1a9169cddbb159416a03fe44510191cd8fe8f869
maragao <maragao@student.42.rio>
2022-12-11 14:10:58 -0800

    update radix

--- radix.c
+++ radix.c
@@ -6,7 +6,7 @@
 /*   By: maragao <maragao@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 22:25:02 by maragao           #+#    #+#             */
-/*   Updated: 2022/12/11 13:33:47 by maragao          ###   ########.fr       */
+/*   Updated: 2022/12/11 14:09:46 by maragao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 void	radix_sort(int argc, t_list **stack_a, t_list **stack_b)
 {
 //	t_list	*temp;
-	t_list	*temp_a;
 	int	i;
 //	int	test;
 	int	count;
 	int	size_b;
 
 	i = 8;
-	temp_a = *stack_a;
-	while (i >= 7)
-	{
+//	while (i >= 7)
+//	{
 /*		test = 1;
 		temp = *stack_a;
 */		count = 0;
@@ -40,21 +38,20 @@ void	radix_sort(int argc, t_list **stack_a, t_list **stack_b)
 */		size_b = 0;
 		while (count < argc - 1)
 		{
-			if (temp_a->bin[i] == '0')
+			if ((*stack_a)->bin[i] == '0')
 			{
-				push_b(&temp_a, stack_b);
+				push_b(stack_a, stack_b);
 				size_b++;
 			}
-			else if (temp_a->bin[8] == '1')
-				rotate_a(&temp_a);
+			else if ((*stack_a)->bin[8] == '1')
+				rotate_a(stack_a);
 			count++;
 		}
 		while (size_b > 0)
 		{
-			push_a(&temp_a, stack_b);
-			
+			push_a(stack_a, stack_b);
 			size_b--;
 		}
-		i--;
-	}
+//		i--;
+//	}
 }
